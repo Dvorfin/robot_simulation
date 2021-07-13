2726ca95453b9257c3fb8c46b3729e6e2428a7db
Dvorfin <70969469+Dvorfin@users.noreply.github.com>
2021-07-13 11:41:26 +0300

    Update pub.py

--- robot_v5/HMI/pub.py
+++ robot_v5/HMI/pub.py
@@ -5,15 +5,7 @@ mqttBroker = "mqtt.eclipseprojects.io"
 client = mqtt.Client("HMI")
 client.connect(mqttBroker)
 
-i = 0
-
-
 def publish(msg):
     client.publish("Commands", msg)
     print("Just published " + msg + " to Topic Commands")
     time.sleep(0.1)
-
-def num():
-    global i
-    i += 1
-    client.publish("Commands", i)
