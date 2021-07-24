80a1b051b69f2f057820047e4e0d7a3492f8e29c
Dvorfin <70969469+Dvorfin@users.noreply.github.com>
2021-07-24 14:35:00 +0300

    Update pub.py

--- robot_v6/HMI/pub.py
+++ robot_v6/HMI/pub.py
@@ -10,8 +10,3 @@ def publish(msg):
     client.publish("Commands", msg)
     print("Just published " + msg + " to Topic Commands")
     time.sleep(0.1)
-
-def num():
-    global i
-    i += 1
-    client.publish("Commands", i)
